1f2f9d7bfcc07e28b7e6bb8aae373eb1a6d56376
HanYang Ding <cqut11403070337@163.com>
2018-06-12 23:13:55 +0800

    test

--- PieGame/run.py
+++ PieGame/run.py
@@ -2,18 +2,26 @@ import pygame
 import sys
 from pygame.locals import *
 
-pygame.init()
-screen = pygame.display.set_mode((600,500))
-myfont = pygame.font.Font(None,60)
-white = 255,255,255
-blue = 0,0,255
-text_image = myfont.render('hellp pygame',True,white)
-
+pygame.init()  # 初始化pygame
+screen = pygame.display.set_mode((600, 500))
+myfont = pygame.font.Font(None, 60)
+white = 255, 255, 255
+blue = 0, 0, 255
+# 第二个参数是抗锯齿
+text_image = myfont.render('hello pygame', True, white)
 
 while 1:
     for event in pygame.event.get():
-        if event.type in (QUIT,KEYDOWN):
+        if event.type in (QUIT, KEYDOWN):
             sys.exit()
     screen.fill(blue)
     screen.blit(text_image, (100, 100))
+
+    # draw a circle
+    color = 255, 255, 0
+    position = 300, 250
+    radius = 100
+    width = 10
+    pygame.draw.circle(screen, color, position, radius, width)
+
     pygame.display.update()
